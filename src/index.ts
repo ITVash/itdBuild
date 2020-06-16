a23c8cd37789741b237dabef0125177e5075c841
Vash <vash_dn@mail.ru>
2020-06-16 07:20:24 +0300

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import http from 'http'
+//import http from 'http'
 
 import { CreateRoutes, connect } from './core'
 
@@ -9,19 +9,19 @@ dotenv.config()
 connect()
 CreateRoutes(app)
 
-const serv = http.createServer(app)
+//const serv = http.createServer(app)
 
 const PORT:number | any = process.env.PORT || 5051
 //const host: string = 'api.itd.company'
 const host:string = 'localhost'
-/* app.listen(PORT, host, () => {
+app.listen(PORT, host, () => {
   console.log(`Сервер запущен по адресу: ${host}:${PORT}`)
-}) */
-serv.listen(PORT, host, () => {
+})
+/* serv.listen(PORT, host, () => {
   console.log(`Сервер запущен по адресу: http://${host}:${PORT}`)
   //console.log('Адресс: ', serv.address().address)
 })
 serv.on('listening', () => {
   const address = serv.address()
   console.log('Listen: ', address)
-})
+}) */
