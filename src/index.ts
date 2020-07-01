2a4f1007b5dc58b4c65e444924931ef21f2923be
ITVash <vash_dn@mail.ru>
2020-07-01 11:52:58 +0300

    Fix Upd

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+//import subdomain from 'express-subdomain'
 import dotenv from 'dotenv'
 //import http from 'http'
 
@@ -13,9 +14,10 @@ CreateRoutes(app)
 
 const PORT:number | any = process.env.PORT || 5051
 //const host: string = 'api.itd.company'
-const host:string = 'localhost'
-app.listen(PORT, host, () => {
+const host:string = '192.168.0.76'
+app.listen(PORT, host, (e) => {
   console.log(`Сервер запущен по адресу: ${host}:${PORT}`)
+  console.log('e', e)
 })
 /* serv.listen(PORT, host, () => {
   console.log(`Сервер запущен по адресу: http://${host}:${PORT}`)
